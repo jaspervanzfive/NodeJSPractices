bd469ff52085ba3dc10c38fefa755a6ed6a00fae
jaspervanzfive <jasperrecitas@SP-JRecitas-MacbookPro2019.local>
2019-10-21 21:48:19 +0800

    Stuffs for pushing array in expressJS , pre-PUG lecture

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,23 @@
 const path = require('path');
-const rootDir = require('../util/path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
-router.post('/product',(req,res,next) => {   
-    console.log(req.body);
-    res.redirect('/');
-    
+const products = [];
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.get('/add-product',(req,res,next) => {
-    res.sendfile(path.join(rootDir,'views','add-product.html'));
-    //res.sendFile('./views/add-product.html');
-    //res.send('<form action="/admin/product" method="POST"><input type="text" name="title"></input><button type="submit">Add product</button></form>');
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
